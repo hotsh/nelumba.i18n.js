2db775ef3c822a9284441f5462ed6644b1f26a4e
wilkie <wilkie05@gmail.com>
2013-11-28 06:44:35 -0500

    Adds implementation and english and spanish rulesets.

---
+++ bin/lotus.i18n.js
@@ -0,0 +1,453 @@
+if (!("lotus" in window)) {
+  window.lotus = (function () {
+    var lotus = {
+    };
+
+    return lotus;
+  }());
+}
+
+if (!("i18n" in lotus)) {
+  lotus.i18n = {}
+}
+
+if (!("raw_lexicons" in lotus.i18n)) {
+  lotus.i18n.raw_lexicons = {}
+}
+
+if (!("lexicons" in lotus.i18n)) {
+  lotus.i18n.lexicons = {}
+}
+
+if (!("raw_grammars" in lotus.i18n)) {
+  lotus.i18n.raw_grammars = {}
+}
+
+if (!("grammars" in lotus.i18n)) {
+  lotus.i18n.grammars = {}
+}
+
+if (!("rules" in lotus.i18n)) {
+  lotus.i18n.rules = {}
+}
+
+if (!('indexOf' in Array.prototype)) {
+  Array.prototype.indexOf= function(find, i /*opt*/) {
+    if (i===undefined) i= 0;
+    if (i<0) i+= this.length;
+    if (i<0) i= 0;
+    for (var n= this.length; i<n; i++)
+      if (i in this && this[i]===find)
+        return i;
+    return -1;
+  };
+}
+if (!Object.keys) {
+  Object.keys = function (obj) {
+    var keys = [],
+        k;
+    for (k in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, k)) {
+        keys.push(k);
+      }
+    }
+    return keys;
+  };
+}
+if (!('forEach' in Array.prototype)) {
+  Array.prototype.forEach= function(action, that /*opt*/) {
+    for (var i= 0, n= this.length; i<n; i++)
+      if (i in this)
+        action.call(that, this[i], i, this);
+  };
+}
+if (!('map' in Array.prototype)) {
+  Array.prototype.map= function(mapper, that /*opt*/) {
+    var other= new Array(this.length);
+    for (var i= 0, n= this.length; i<n; i++)
+      if (i in this)
+        other[i]= mapper.call(that, this[i], i, this);
+    return other;
+  };
+}
+if (!('filter' in Array.prototype)) {
+  Array.prototype.filter= function(filter, that /*opt*/) {
+    var other= [], v;
+    for (var i=0, n= this.length; i<n; i++)
+      if (i in this && filter.call(that, v= this[i], i, this))
+        other.push(v);
+    return other;
+  };
+}
+if (!('every' in Array.prototype)) {
+  Array.prototype.every= function(tester, that /*opt*/) {
+    for (var i= 0, n= this.length; i<n; i++)
+      if (i in this && !tester.call(that, this[i], i, this))
+        return false;
+    return true;
+  };
+}
+
+lotus.i18n.lexicon = function(options) {
+  var locale = lotus.i18n.locale(options);
+
+  if (locale in lotus.i18n.lexicons) {
+    return lotus.i18n.lexicons[locale];
+  }
+
+  lotus.i18n.lexicons[locale] = YAML.parse(lotus.i18n.raw_lexicons[locale])[locale];
+  return lotus.i18n.lexicons[locale];
+}
+
+lotus.i18n.grammar = function(options) {
+  var locale = lotus.i18n.locale(options);
+
+  if (locale in lotus.i18n.grammars) {
+    return lotus.i18n.grammars[locale];
+  }
+
+  lotus.i18n.grammars[locale] = YAML.parse(lotus.i18n.raw_grammars[locale])[locale];
+  return lotus.i18n.grammars[locale];
+};
+
+lotus.i18n.rules = function(options) {
+  var locale = lotus.i18n.locale(options);
+
+  if (locale in lotus.i18n.rules) {
+    return lotus.i18n.rules[locale];
+  }
+
+  var grammar = lotus.i18n.grammar(options);
+  var rules = [];
+
+  function cartProd(paramArray) {
+    function addTo(curr, args) {
+      var i, copy,
+          rest = args.slice(1),
+          last = !rest.length,
+          result = [];
+
+      for (i = 0; i < args[0].length; i++) {
+        copy = curr.slice();
+        copy.push(args[0][i]);
+
+        if (last) {
+          result.push(copy);
+        } else {
+          result = result.concat(addTo(copy, rest));
+        }
+      }
+
+      return result;
+    }
+
+    return addTo([], Array.prototype.slice.call(arguments));
+  }
+
+  var number_of_rules = grammar["rules"].length;
+  for (var i = 0; i < number_of_rules; i++) {
+    var rule = grammar["rules"][i];
+
+    if (rule["match"] && rule["match"].length > 0 &&
+        !(typeof(rule["match"][0]) == "object")) {
+      rule["match"] = [rule["match"]];
+    }
+
+    if (rule["do"].match(/\$[^\$]+\$/)) {
+      var patterns = rule["do"].split('$');
+      if ((patterns.length % 2) == 0) {
+        patterns.push("");
+      }
+      var number_of_subrules = (patterns.length - 1) / 2;
+      var subrules = [];
+      for (var j = 1; j < patterns.length; j+=2) {
+        subrules.push(grammar["subrules"][patterns[j]]);
+      }
+
+      subrules = cartProd.apply(null, subrules);
+
+      for (var j = 0; j < subrules.length; j++) {
+        var set = subrules[j];
+        var new_rule = {};
+
+        new_rule["for"] = [];
+        for (var k = 0; k < set.length; k++) {
+          new_rule["for"] = new_rule["for"].concat(set[k]["for"]);
+        }
+        new_rule["for"] = new_rule["for"].concat(rule["for"]);
+        // Remove duplicates and nulls:
+        new_rule["for"] = new_rule["for"].filter(function(elem, pos, self) {
+          if (elem == null) {
+            return false;
+          }
+          return self.indexOf(elem) == pos;
+        });
+
+        new_rule["match"] = [];
+        for (var k = 0; k < set.length; k++) {
+          var matches = set[k]["match"];
+          if (matches && matches.length > 0 &&
+              !(typeof(matches[0]) == "object")) {
+            matches = [matches];
+          }
+          if (matches) {
+            new_rule["match"] = new_rule["match"].concat(matches);
+          }
+        }
+
+        if (new_rule["match"].length == 0) {
+          new_rule["match"].push([]);
+        }
+
+        new_rule["do"] = "";
+        for (var k = 0; k < patterns.length; k++) {
+          if ((k % 2) == 0) {
+            new_rule["do"] += patterns[k];
+          }
+          else {
+            new_rule["do"] += set[(k-1)/2]["do"];
+          }
+        }
+
+        rules.push(new_rule);
+      }
+    }
+    else {
+      if (!rule["for"]) {
+        rule["for"] = [];
+      }
+
+      if (!rule["match"]) {
+        rule["match"] = [[]];
+      }
+
+      rules.push(rule);
+    }
+  }
+
+  lotus.i18n.rules[locale] = rules;
+
+  return lotus.i18n.rules[locale];
+};
+
+lotus.i18n.translate = function(tag, options) {
+  if (options == undefined) {
+    options = {};
+  }
+
+  var locale = lotus.i18n.locale(options);
+  var lexicon = lotus.i18n.lexicon(options);
+
+  function pathTo(obj,is) {
+    function multiIndex(obj,is) {
+      return is.length ? multiIndex(obj[is[0]],is.slice(1)) : obj
+    }
+    return multiIndex(obj,is.split('.'))
+  }
+
+  return pathTo(lexicon, tag);
+};
+
+lotus.i18n.sentence = function(options) {
+  var locale = lotus.i18n.locale(options);
+  var rules = lotus.i18n.rules(options);
+  var lexicon = lotus.i18n.lexicon(options);
+
+  var components = {};
+  var elements = [];
+
+  if (typeof(options["actor"]) == "object") {
+    components["actor_pronoun"] = options["actor"];
+    elements.push("actor_pronoun");
+  }
+  else if (options["actor_pronoun"]) {
+    components["actor_pronoun"] = options["actor_pronoun"];
+    elements.push("actor_pronoun");
+  }
+  else if (options["actor"]) {
+    components["actor"] = options["actor"];
+    elements.push("actor");
+  }
+
+  if (typeof(options["actors"]) == "object") {
+    components["actors_pronoun"] = options["actors"];
+    elements.push("actors_pronoun");
+  }
+  else if (options["actors_pronoun"]) {
+    components["actors_pronoun"] = options["actors_pronoun"];
+    elements.push("actors_pronoun");
+  }
+  else if (options["actors"]) {
+    components["actors"] = options["actors"];
+    elements.push("actors");
+  }
+
+  if (typeof(options["object_owner"]) == "object") {
+    components["object_owner_pronoun"] = options["object_owner"];
+    elements.push("object_owner_pronoun");
+  }
+  else if (options["object_owner_pronoun"]) {
+    components["object_owner_pronoun"] = options["object_owner_pronoun"];
+    elements.push("object_owner_pronoun");
+  }
+  else if (options["object_owner"]) {
+    components["object_owner"] = options["object_owner"];
+    elements.push("object_owner");
+  }
+
+  if (typeof(options["object_owners"]) == "object") {
+    components["object_owners_pronoun"] = options["object_owners"];
+    elements.push("object_owners_pronoun");
+  }
+  else if (options["object_owners_pronoun"]) {
+    components["object_owners_pronoun"] = options["object_owners_pronoun"];
+    elements.push("object_owners_pronoun");
+  }
+  else if (options["object_owners"]) {
+    components["object_owners"] = options["object_owners"];
+    elements.push("object_owners");
+  }
+
+  if (typeof(options["target_owner"]) == "object") {
+    components["target_owner_pronoun"] = options["target_owner"];
+    elements.push("target_owner_pronoun");
+  }
+  else if (options["target_owner_pronoun"]) {
+    components["target_owner_pronoun"] = options["target_owner_pronoun"];
+    elements.push("target_owner_pronoun");
+  }
+  else if (options["target_owner"]) {
+    components["target_owner"] = options["target_owner"];
+    elements.push("target_owner");
+  }
+
+  if (typeof(options["target_owners"]) == "object") {
+    components["target_owners_pronoun"] = options["target_owners"];
+    elements.push("target_owners_pronoun");
+  }
+  else if (options["target_owners_pronoun"]) {
+    components["target_owners_pronoun"] = options["target_owners_pronoun"];
+    elements.push("target_owners_pronoun");
+  }
+  else if (options["target_owners"]) {
+    components["target_owners"] = options["target_owners"];
+    elements.push("target_owners");
+  }
+
+  if (options["action"]) {
+    components["action"] = options["action"];
+    elements.push("action");
+  }
+
+  if (options["field"]) {
+    components["field"] = options["field"];
+    elements.push("field");
+  }
+
+  if (options["object"]) {
+    components["object"] = options["object"];
+    elements.push("object");
+  }
+
+  if (options["objects"]) {
+    components["objects"] = options["objects"];
+    elements.push("objects");
+  }
+
+  if (options["target"]) {
+    components["target"] = options["target"];
+    elements.push("target");
+  }
+
+  if (options["targets"]) {
+    components["targets"] = options["targets"];
+    elements.push("targets");
+  }
+
+  if (options["verb"]) {
+    components["verb"] = options["verb"];
+    elements.push("verb");
+  }
+
+  var result = "";
+  rules.every(function(hash, i, arr) {
+    var unused = hash["for"].filter(function(elem, i, self) {
+      return !(elem in components)
+    });
+
+    if (hash["for"].indexOf("actor") != -1 && hash["for"].indexOf("verb") != -1) {
+    }
+
+    if (unused.length == 0 && hash["for"].length == elements.length) {
+      if (!hash["match"]) {
+        hash["match"] = [[]];
+      }
+
+      var violations = hash["match"].filter(function(rule, i, self) {
+        if (rule[0] && components[rule[0]]) {
+          var value = components[rule[0]];
+          if (typeof(value) == "object") {
+            value = value.value;
+          }
+          return !value.match(new RegExp(rule[1]));
+        }
+        else {
+          return false;
+        }
+      });
+
+      if (violations.length == 0) {
+        result = hash["do"];
+
+        elements.forEach(function(component, i, self) {
+          var value = components[component];
+          if (typeof(value) == "object") {
+            value = value.value;
+            var matcher = new RegExp("[\\.a-zA-Z_]*%"
+              + component.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1")
+              + "%[\\.a-zA-Z_]*");
+
+            token = result.match(matcher);
+            token = token[0].replace("%"+component+"%", value);
+            result = result.replace(matcher, lotus.i18n.translate(token, options));
+          }
+          else {
+            result = result.replace("%"+component+"%", value);
+          }
+        });
+
+        return false;
+      }
+    }
+
+    return true;
+  });
+
+  return result;
+};
+
+lotus.i18n.locale = function(options) {
+  var locale = options["locale"];
+
+  if (!locale) {
+    locale = "en";
+  }
+
+  return locale;
+};
+
+lotus.i18n.verbs = {
+  post: { value: "post" }
+};
+
+lotus.i18n.objects = {
+  note:  { value: "note" },
+  album: { value: "album" },
+  file:  { value: "file" }
+};
+
+lotus.i18n.pronouns = {
+  he:  { value: "he" },
+  she: { value: "she" },
+  ey:  { value: "ey" }
+};
---
+++ demo/app.js
@@ -0,0 +1,135 @@
+function update() {
+  var options = {};
+
+  var pronoun_dropdown = document.getElementById("actor_pronoun");
+  var pronoun = pronoun_dropdown.options[pronoun_dropdown.selectedIndex].value;
+  if (pronoun != "none") {
+    if (document.getElementById("actor_plural").checked) {
+      options.actors = {value: pronoun};
+    }
+    else {
+      options.actor = {value: pronoun};
+    }
+  }
+  else {
+    var actor = document.getElementById("actor").value;
+    if (actor != "") {
+      if (document.getElementById("actor_plural").checked) {
+        options.actors = actor;
+      }
+      else {
+        options.actor = actor;
+      }
+    }
+  }
+
+  var verb_dropdown = document.getElementById("verb");
+  var verb = verb_dropdown.options[verb_dropdown.selectedIndex].value;
+  if (verb != "none") {
+    options.verb = {value: verb};
+  }
+
+  var object_dropdown = document.getElementById("object");
+  var object = object_dropdown.options[object_dropdown.selectedIndex].value;
+  if (object != "none") {
+    if (document.getElementById("object_plural").checked) {
+      options.objects = {value: object};
+    }
+    else {
+      options.object = {value: object};
+    }
+  }
+
+  var target_dropdown = document.getElementById("target");
+  var target = target_dropdown.options[target_dropdown.selectedIndex].value;
+  if (target != "none") {
+    if (document.getElementById("target_plural").checked) {
+      options.targets = {value: target};
+    }
+    else {
+      options.target = {value: target};
+    }
+  }
+
+  pronoun_dropdown = document.getElementById("object_owner_pronoun");
+  pronoun = pronoun_dropdown.options[pronoun_dropdown.selectedIndex].value;
+  if (pronoun != "none") {
+    if (document.getElementById("object_owner_plural").checked) {
+      options.object_owners = {value: pronoun};
+    }
+    else {
+      options.object_owner = {value: pronoun};
+    }
+  }
+  else {
+    var object_owner = document.getElementById("object_owner").value;
+    if (object_owner != "") {
+      if (document.getElementById("object_owner_plural").checked) {
+        options.object_owners = object_owner;
+      }
+      else {
+        options.object_owner = object_owner;
+      }
+    }
+  }
+
+  pronoun_dropdown = document.getElementById("target_owner_pronoun");
+  pronoun = pronoun_dropdown.options[pronoun_dropdown.selectedIndex].value;
+  if (pronoun != "none") {
+    if (document.getElementById("target_owner_plural").checked) {
+      options.target_owners = {value: pronoun};
+    }
+    else {
+      options.target_owner = {value: pronoun};
+    }
+  }
+  else {
+    var target_owner = document.getElementById("target_owner").value;
+    if (target_owner != "") {
+      if (document.getElementById("target_owner_plural").checked) {
+        options.target_owners = target_owner;
+      }
+      else {
+        options.target_owner = target_owner;
+      }
+    }
+  }
+
+  var result = lotus.i18n.sentence(options);
+  document.getElementById("output_en").innerHTML = result;
+
+  options.locale = "es";
+  result = lotus.i18n.sentence(options);
+  document.getElementById("output_es").innerHTML = result;
+}
+
+document.getElementById("actor").onkeyup = update;
+document.getElementById("actor").onchange = update;
+document.getElementById("actor_plural").onkeyup = update;
+document.getElementById("actor_plural").onchange = update;
+document.getElementById("object_plural").onkeyup = update;
+document.getElementById("object_plural").onchange = update;
+document.getElementById("target_plural").onkeyup = update;
+document.getElementById("target_plural").onchange = update;
+document.getElementById("target").onkeyup = update;
+document.getElementById("target").onchange = update;
+document.getElementById("object").onkeyup = update;
+document.getElementById("object").onchange = update;
+document.getElementById("verb").onkeyup = update;
+document.getElementById("verb").onchange = update;
+document.getElementById("object_owner_plural").onkeyup = update;
+document.getElementById("object_owner_plural").onchange = update;
+document.getElementById("target_owner_plural").onkeyup = update;
+document.getElementById("target_owner_plural").onchange = update;
+document.getElementById("target_owner").onkeyup = update;
+document.getElementById("target_owner").onchange = update;
+document.getElementById("object_owner").onkeyup = update;
+document.getElementById("object_owner").onchange = update;
+document.getElementById("object_owner_pronoun").onkeyup = update;
+document.getElementById("object_owner_pronoun").onchange = update;
+document.getElementById("target_owner_pronoun").onkeyup = update;
+document.getElementById("target_owner_pronoun").onchange = update;
+document.getElementById("actor_pronoun").onkeyup = update;
+document.getElementById("actor_pronoun").onchange = update;
+
+update();
